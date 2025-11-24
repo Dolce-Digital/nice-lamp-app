5d370037797645c85e6c0dac30444263c38df740
Dolce-Digital <sonja@dolce-digital-va.com>
2025-11-24 15:49:41 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
   import "../app.css";
 </script>
+
+<slot />
+
 <footer style="text-align:center; padding:2rem; opacity:0.6; font-size:0.9rem;">
   Nice-Lamp ConciergeEmail • © 2025 • Made with love & coffee ☕
 </footer>
