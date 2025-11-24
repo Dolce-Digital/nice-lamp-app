233502e1ed90355e8d2c082a783725b52c17d210
Dolce-Digital <sonja@dolce-digital-va.com>
2025-11-24 11:07:38 +0100

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  // future logic goes here
+</script>
+
+<style>
+  body {
+    background: var(--nl-light);
+    font-family: Raleway, sans-serif;
+    color: var(--nl-dark);
+  }
+</style>
+
+<div class="app-container">
+  <h1>Nice-Lamp ConciergeEmail App</h1>
+  <p>Welcome to your App V1 — UI coming next!</p>
+</div>
