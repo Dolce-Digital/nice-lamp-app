ee2aa8a43b7f34c8eeb631ccd485079a2f78eda2
Dolce-Digital <sonja@dolce-digital-va.com>
2025-11-24 14:32:45 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,17 +2,15 @@
   import Nav from "./components/Nav.svelte";
   import Home from "./pages/Home.svelte";
   import NewEmail from "./pages/NewEmail.svelte";
+  import Packs from "./pages/Packs.svelte";
 
-  // simple routing until we add a router library
   let route = window.location.pathname;
 
-  // react to clicks
   const navigate = (path) => {
     history.pushState({}, "", path);
     route = path;
   };
 
-  // handle back/forward
   window.onpopstate = () => {
     route = window.location.pathname;
   };
@@ -22,6 +20,8 @@
 
 {#if route === "/new"}
   <NewEmail />
+{:else if route === "/packs"}
+  <Packs />
 {:else}
   <Home />
 {/if}
